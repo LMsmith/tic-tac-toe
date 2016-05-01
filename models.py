bdf092849e2f140d2bcff91512944761716aadd4
Lauren Smith <LMSmith.715@gmail.com>
2016-05-01 15:29:39 -0400

    add files

---
+++ models.py
@@ -0,0 +1,104 @@
+"""models.py - This file contains the class definitions for the Datastore
+entities used by the Game. Because these classes are also regular Python
+classes they can include methods (such as 'to_form' and 'new_game')."""
+
+import random
+from datetime import date
+from protorpc import messages
+from google.appengine.ext import ndb
+
+
+class User(ndb.Model):
+    """User profile"""
+    name = ndb.StringProperty(required=True)
+    email =ndb.StringProperty()
+
+
+class Game(ndb.Model):
+    """Game object"""
+    open_moves = ndb.JsonProperty(required=True)
+    x_moves = ndb.JsonProperty(required=True)
+    o_moves = ndb.JsonProperty(required=True)
+    game_over = ndb.BooleanProperty(required=True, default=False)
+    user = ndb.KeyProperty(required=True, kind='User')
+
+    @classmethod
+    def new_game(cls, user):
+        """Creates and returns a new game"""
+        game = Game(user=user,
+                    open_moves=list(range(1, 10)),
+                    x_moves=list(),
+                    o_moves=list(),
+                    game_over=False)
+        game.put()
+        return game
+
+    def to_form(self, message):
+        """Returns a GameForm representation of the Game"""
+        form = GameForm()
+        form.urlsafe_key = self.key.urlsafe()
+        form.user_name = self.user.get().name
+        form.open_moves = self.open_moves
+        form.x_moves = self.x_moves
+        form.o_moves = self.o_moves
+        form.game_over = self.game_over
+        form.message = message
+        return form
+
+    def end_game(self, won=False):
+        """Ends the game - if won is True, the player won. - if won is False,
+        the player lost."""
+        self.game_over = True
+        self.put()
+        # Add the game to the score 'board'
+        score = Score(user=self.user, date=date.today(), won=won)
+        score.put()
+
+
+class Score(ndb.Model):
+    """Score object"""
+    user = ndb.KeyProperty(required=True, kind='User')
+    date = ndb.DateProperty(required=True)
+    won = ndb.BooleanProperty(required=True)
+
+    def to_form(self):
+        return ScoreForm(user_name=self.user.get().name, won=self.won,
+                         date=str(self.date), guesses=self.guesses)
+
+
+class GameForm(messages.Message):
+    """GameForm for outbound game state information"""
+    urlsafe_key = messages.StringField(1, required=True)
+    open_moves = messages.StringField(2, required=True)
+    x_moves = messages.StringField(3, required=True)
+    o_moves = messages.StringField(4, required=True)
+    game_over = messages.BooleanField(5, required=True)
+    message = messages.StringField(6, required=True)
+    user_name = messages.StringField(7, required=True)
+
+
+class NewGameForm(messages.Message):
+    """Used to create a new game"""
+    user_name = messages.StringField(1, required=True)
+
+
+class MakeMoveForm(messages.Message):
+    """Used to make a move in an existing game"""
+    move = messages.IntegerField(1, required=True)
+
+
+class ScoreForm(messages.Message):
+    """ScoreForm for outbound Score information"""
+    user_name = messages.StringField(1, required=True)
+    date = messages.StringField(2, required=True)
+    won = messages.BooleanField(3, required=True)
+
+
+class ScoreForms(messages.Message):
+    """Return multiple ScoreForms"""
+    items = messages.MessageField(ScoreForm, 1, repeated=True)
+
+
+class StringMessage(messages.Message):
+    """StringMessage-- outbound (single) string message"""
+    message = messages.StringField(1, required=True)
